f4e9db2d1e6db784584af1c88eb44da12223712c
JIWON <26162616@naver.com>
2020-11-04 22:16:56 +0900

    merge base

--- web/artproject/mainapp/models.py
+++ web/artproject/mainapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     name=models.CharField(max_length=50)
-    item=models.CharField(max_length=50)
+    item=models.CharField(max_length=50) # 카테고리 
     public= models.BooleanField(default=False)
     rating= models.IntegerField()
     date=models.DateField(null=True, blank=True)
