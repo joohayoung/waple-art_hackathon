df5ac2529e048b2ac1f9e93f3b690f8d035c0834
JIWON <26162616@naver.com>
2020-11-04 19:42:47 +0900

    subapp update

--- web/artproject/subapp/admin.py
+++ web/artproject/subapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import ArtInfoDB, PerformanceDB, FestivalDB, ArtWorkDB
 # Register your models here.
+
+class ArtInfoAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'category',)
+
+class PerformanceAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'basic_title','cl',)
+
+class FestivalAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'basic_title',)
+
+class ArkWorkAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'basic_title','cl',)
+
+admin.site.register(ArtInfoDB, ArtInfoAdmin)
+admin.site.register(PerformanceDB, PerformanceAdmin)
+admin.site.register(FestivalDB, FestivalAdmin)
+admin.site.register(ArtWorkDB, ArkWorkAdmin)
