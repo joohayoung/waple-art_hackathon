b39224bf28b931f7db00cc1a44d20a22d8a447c3
unknown <hayjoo0723@gmail.com>
2020-11-04 19:39:11 +0900

    art

--- web/artproject/mainapp/models.py
+++ web/artproject/mainapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    name=models.CharField(max_length=50)
+    item=models.CharField(max_length=50)
+    public= models.BooleanField(default=False)
+    rating= models.IntegerField()
+    date=models.DateField(null=True, blank=True)
+    image=models.ImageField()
+    comment=models.TextField()
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
